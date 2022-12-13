3b916139053dc15a5bf7fcb9529d4b033e180f5e
Diego Hernández <godie8a@hotmail.com>
2022-12-13 16:13:06 -0500

    Update posts_controller_spec.rb

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template(:index)
     end
 
-    it 'should render the correct text in the template' do
+    it 'should render the correct text in the template.' do
       expect(response.body).to include('This is the Posts index page.')
     end
   end
