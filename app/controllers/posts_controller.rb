7b8099f218c0972958fa862578ab610b377f11d9
Oluyemi Paul <oluola96@gmail.com>
2022-12-14 16:52:57 +0100

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create delete]
+
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @comments = @post.comments
+    @comments = @post.comments.includes(:author)
   end
 
   def new
@@ -19,11 +21,15 @@ class PostsController < ApplicationController
     @post = Post.new(posts_params)
     @post.author = current_user
     if @post.save
+      flash[:notice] = 'Post published succesfully'
       redirect_to user_post_path(current_user, @post)
     else
+      flash[:error] = @post.errors.full_messages[0]
       render :new
     end
   end
+
+  private
 
   def posts_params
     params.require(:post).permit(:title, :text)
