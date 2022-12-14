419e6ffd7356e9ed31f3ad817300ec9581f872c3
Oluyemi Paul <oluola96@gmail,com>
2022-12-14 09:01:19 +0100

    update test for post

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before :each do
+    @user = User.create(name: 'Augusto', photo: 'Photo', bio: 'Hello world')
+    @post = @user.posts.create(title: 'Test post', text: 'This is a test', author_id: @user.id)
+  end
+
   describe 'GET /index' do
-    before(:each) { get user_posts_path(6) }
+    before { get user_posts_path(@user, @post) }
 
     it 'should return 200 success' do
       expect(response).to have_http_status(200)
     end
 
-    it 'should render the index template.' do
+    it 'should render the index template' do
       expect(response).to render_template(:index)
     end
 
-    it 'should render the correct text in the template.' do
-      expect(response.body).to include('This is the Posts index page.')
+    it 'should render the correct text in the template' do
+      expect(response.body).to include('Test post')
     end
   end
 
   describe 'GET /show' do
-    before(:each) { get user_post_path(6, 1) }
+    before { get user_post_path(@user, @post) }
 
     it 'should return 200 success' do
       expect(response).to have_http_status(200)
@@ -29,7 +34,7 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'should render the correct text in the template' do
-      expect(response.body).to include('This is the Posts show page.')
+      expect(response.body).to include('This is a test')
     end
   end
 end
