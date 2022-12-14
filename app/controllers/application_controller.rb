c4b037d804f8203c3f85e09f8e7a6b2dabf8a7a7
Oluyemi Paul <oluola96@gmail,com>
2022-12-14 17:17:58 +0100

    resolve err

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
- protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+
   before_action :update_allowed_parameters, if: :devise_controller?
+
   protected
+
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
@@ -9,4 +12,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :photo, :bio, :email, :password, :current_password)
     end
+  end
 end
